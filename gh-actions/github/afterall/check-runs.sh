eefe1a06bf7a543d309bdcbd4c7943bc60cc53b6
Ryan Northey <ryan@synca.io>
2025-12-19 07:09:05 +0000

    gh-actions/

--- gh-actions/github/afterall/check-runs.sh
+++ gh-actions/github/afterall/check-runs.sh
@@ -7,7 +7,7 @@ OUTPUT=$(gh api --jq "${SCRIPT_JQ}" "/repos/${REPO}/actions/runs?page=${PAGE}&he
 
 for PAGE in $(seq 1 "${MAX_PAGES}"); do
     echo "CHECK PAGE: ${PAGE}" >&2
-    # PAGE_OUTPUT=$(gh api --jq "${SCRIPT_JQ}" "/repos/${REPO}/actions/runs?page=${PAGE}&head_sha=${HEAD_SHA}&status=completed&per_page=${PER_PAGE}")
+    PAGE_OUTPUT=$(gh api --jq "${SCRIPT_JQ}" "/repos/${REPO}/actions/runs?page=${PAGE}&head_sha=${HEAD_SHA}&status=completed&per_page=${PER_PAGE}")
     # OUTPUT=$(jq -c --argjson a "$OUTPUT" --argjson b "$PAGE_OUTPUT" '$a+$b')
 done
 
