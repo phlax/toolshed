bcd251e0f4e070115a12ee2975d6ee9f31c2192d
Ryan Northey <ryan@synca.io>
2025-12-19 06:46:38 +0000

    gh-actions/

--- gh-actions/github/afterall/check-runs.sh
+++ gh-actions/github/afterall/check-runs.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-set -o pipefail
+set -e -o pipefail
 
 gh api --jq "${SCRIPT_JQ}" "/repos/${REPO}/actions/runs?page=${PAGE}&head_sha=${HEAD_SHA}&status=completed&per_page=${PER_PAGE}"
